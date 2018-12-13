119e1a2987231dc34728cc2f8611127d850e9d68
Thomas Hamburger <hamburgerthomas@gmail.com>
2018-12-13 15:15:51 +0100

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Thomas Hamburger on 13/12/2018.
+//  Copyright © 2018 Thomas Hamburger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
