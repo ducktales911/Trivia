d37f4c4ed6bcc781dc17bc03f493fc744ff8a1fd
Thomas Hamburger <hamburgerthomas@gmail.com>
2019-02-12 11:23:50 +0100

    Podfile met HTMLstring library toegevoegd

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -10,7 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet var answerButtons: [UIButton]!
+    
+    let questionURL = "http://opentdb.com/api.php?amount=10"
+    
+    
+    
+    
+    
     override func viewDidLoad() {
+        for button in answerButtons {
+            button.setTitle("hi", for: .normal)
+        }
+
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
