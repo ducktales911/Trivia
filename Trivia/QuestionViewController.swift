224798d9bd0e1a9387296bfec9c42021b49af156
Thomas <hamburgerthomas@gmail.com>
2019-05-03 12:05:08 +0200

    Nieuwe vraag en antwoorden worden geladen als op button is getapt.

--- Trivia/QuestionViewController.swift
+++ Trivia/QuestionViewController.swift
@@ -18,6 +18,8 @@ class QuestionViewController: UIViewController {
     
     var items = [QuestionItem]()
     
+    var questionCounter = 0
+    
     func fetchQuestions() {
         self.items = []
         
@@ -32,10 +34,7 @@ class QuestionViewController: UIViewController {
                 if let items = items {
                     self.items = items
                     print(items)
-                    self.questionLabel.text = items[0].question.removingHTMLEntities // laat alleen nog eerste vraag zien
-                    var allAnswers = items[0].incorrectAnswers + [items[0].correctAnswer]
-                    allAnswers.shuffle()
-                    self.updateButtons(answers: allAnswers)
+                    self.nextQuestion(number: self.questionCounter)
                 } else {
                     print("Unable to load data.")
                 }
@@ -44,16 +43,34 @@ class QuestionViewController: UIViewController {
     }
     
     func updateButtons(answers: [String]) {
-        answerButtons[0].setTitle(answers[0].removingHTMLEntities, for: .normal)
-        answerButtons[1].setTitle(answers[1].removingHTMLEntities, for: .normal)
-        answerButtons[2].setTitle(answers[2].removingHTMLEntities, for: .normal)
-        answerButtons[3].setTitle(answers[3].removingHTMLEntities, for: .normal)
         
+        for (index, element) in answers.enumerated() {
+            answerButtons[index].setTitle(element.removingHTMLEntities, for: .normal)
+        }
+//        answerButtons[0].setTitle(answers[0].removingHTMLEntities, for: .normal)
+//        answerButtons[1].setTitle(answers[1].removingHTMLEntities, for: .normal)
+//        answerButtons[2].setTitle(answers[2].removingHTMLEntities, for: .normal)
+//        answerButtons[3].setTitle(answers[3].removingHTMLEntities, for: .normal)
+        
+    }
+    
+    func nextQuestion(number: Int) {
+        self.questionLabel.text = items[number].question.removingHTMLEntities // laat alleen nog eerste vraag zien
+        var allAnswers = items[number].incorrectAnswers + [items[number].correctAnswer]
+        allAnswers.shuffle()
+        self.updateButtons(answers: allAnswers)
     }
     
     
     @IBAction func buttonTapped(_ sender: UIButton) {
         print(sender.title(for: .normal)!)
+        if sender.title(for: .normal)! == items[questionCounter].correctAnswer {
+            print("CORRECT!")
+        } else {
+            print("WRONG ANSWER!")
+        }
+        questionCounter += 1
+        nextQuestion(number: questionCounter)
     }
     
     
@@ -61,6 +78,7 @@ class QuestionViewController: UIViewController {
     
     override func viewDidLoad() {
         questionLabel.text = ""
+        questionCounter = 0
         for button in answerButtons {
             button.setTitle("", for: .normal)
         }
