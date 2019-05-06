23fe133241cccd51e9bef94e6be059033ba75fa4
Thomas <hamburgerthomas@gmail.com>
2019-05-07 00:20:36 +0200

    Verbeteringen in de code en laat nu in een alert zien of het antwoord goed of fout was.

--- Trivia/GamePlayViewController.swift
+++ Trivia/GamePlayViewController.swift
@@ -25,7 +25,7 @@ class GamePlayViewController: UIViewController {
     var questionNumber = 0
     
     func fetchQuestions() {
-        self.allQuestions = []
+        allQuestions = []
         let query = [
             "amount": "10"
         ]
@@ -37,7 +37,7 @@ class GamePlayViewController: UIViewController {
                 if let items = items {
                     self.allQuestions = items
                     print(items)
-                    self.updateUI(number: self.questionNumber)
+                    self.updateUI()
                 } else {
                     print("Unable to load data.")
                 }
@@ -45,13 +45,15 @@ class GamePlayViewController: UIViewController {
         })
     }
     
-    func updateUI(number: Int) {
-        self.prograssBar.frame.size.width = (view.frame.size.width / CGFloat(allQuestions.count)) * CGFloat(questionNumber + 1)
-        self.progressLabel.text = "\(questionNumber + 1) / \(allQuestions.count)"
-        self.questionLabel.text = allQuestions[number].question.removingHTMLEntities // laat alleen nog eerste vraag zien
-        var allAnswers = allQuestions[number].incorrectAnswers + [allQuestions[number].correctAnswer]
-        allAnswers.shuffle()
-        self.updateButtons(answers: allAnswers)
+    func updateUI() {
+            print(questionNumber)
+            prograssBar.frame.size.width = (view.frame.size.width / CGFloat(allQuestions.count)) * CGFloat(questionNumber + 1)
+            progressLabel.text = "\(questionNumber + 1) / \(allQuestions.count)"
+            scoreLabel.text = "Score: \(score)"
+            questionLabel.text = allQuestions[questionNumber].question.removingHTMLEntities // laat alleen nog eerste vraag zien
+            var allAnswers = allQuestions[questionNumber].incorrectAnswers + [allQuestions[questionNumber].correctAnswer]
+            allAnswers.shuffle()
+            updateButtons(answers: allAnswers)
     }
     
     func updateButtons(answers: [String]) {
@@ -71,35 +73,52 @@ class GamePlayViewController: UIViewController {
     }
     
     @IBAction func buttonTapped(_ sender: UIButton) {
-        
-        UIView.animate(withDuration: 0.3) {
-            sender.transform = CGAffineTransform(scaleX: 3.0, y: 3.0)
-            sender.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
-        }
-        
-        if (questionNumber < allQuestions.count) {
-            print(sender.title(for: .normal)!)
-            if sender.title(for: .normal)! == allQuestions[questionNumber].correctAnswer {
-                print("CORRECT!")
-                score += 1
-                scoreLabel.text = "Score: \(score)"
-            } else {
-                print("WRONG ANSWER!")
-            }
-            questionNumber += 1
-        }
-        if (questionNumber < allQuestions.count) {
-            updateUI(number: questionNumber)
+        let selectedAnswer = sender.title(for: .normal)!
+        let correctAnswer = allQuestions[questionNumber].correctAnswer.removingHTMLEntities
+        if (selectedAnswer == correctAnswer) {
+            score += 50
+            showFeedbackAlert(title: "Correct!", message: "", duration: 500)
         } else {
-            showHighScore()
+            score -= 30
+            showFeedbackAlert(title: "Wrong!", message: "The answer was: \(correctAnswer)", duration: 1000)
+        }
+        if (!isLastQuestion()) {
+            questionNumber += 1
+            updateUI()
+        } else {
+            scoreLabel.text = "Score: \(score)"
         }
     }
     
-    func showHighScore() {
-        print(score)
+    func isLastQuestion() -> Bool {
+        return (questionNumber == allQuestions.count - 1)
+    }
+    
+    func postHighScore() {
         let finalScore = HighscoreModel(name: playerName, score: String(score))
         player.postItem(player: finalScore)
-        performSegue(withIdentifier: "showHighscore", sender: answerButtons)
+    }
+    
+    //
+    func showFeedbackAlert(title: String, message: String, duration: Int? = nil) -> Void
+    {
+        let alert = UIAlertController(title: title, message:
+            message, preferredStyle: .alert)
+        
+        if isLastQuestion() {
+            postHighScore()
+            alert.message = message + "\n And your final score is: \n \(score)"
+            let highScoreAction = UIAlertAction(title: "Show Highscore", style: .default) {
+                UIAlertAction in
+                self.performSegue(withIdentifier: "showHighscore", sender: UIAlertAction)
+            }
+            alert.addAction(highScoreAction)
+        } else {
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(duration!)) {
+                alert.dismiss(animated: true, completion: nil)
+            }
+        }
+        present(alert, animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
@@ -118,13 +137,13 @@ class GamePlayViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.navigationController?.isNavigationBarHidden = true
+        navigationController?.isNavigationBarHidden = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        self.navigationController?.isNavigationBarHidden = false
+        navigationController?.isNavigationBarHidden = false
     }
 
 }
